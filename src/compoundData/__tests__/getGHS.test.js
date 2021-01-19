15198e14ee5f442281a555578933f76cbebf0603
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2021-01-19 14:43:45 +0100

    Adding GHS information (#10)
    * chore: playing with getGHS api
    
    * chore: added todo
    
    * chore: added docstrings
    
    * chore: refactored after discussion with Luc

--- src/compoundData/__tests__/getGHS.test.js
+++ src/compoundData/__tests__/getGHS.test.js
@@ -2,28 +2,100 @@ import { getGHS } from '../getGHS.js';
 
 import data from './test.json';
 
-test('getGHS', () => {
+test('getGHS default request', () => {
   let result = getGHS(data);
-  expect(result).toStrictEqual({
-    pictograms: [
-      {
-        label: 'Flammable',
-        url: 'https://pubchem.ncbi.nlm.nih.gov/images/ghs/GHS02.svg',
+  expect(result).toHaveProperty('summary.hCodes');
+  expect(result.summary.hCodes).toStrictEqual([
+    'H226',
+    'H315',
+    'H319',
+    'H335',
+    'H411',
+  ]);
+  expect(result).toHaveProperty('summary.pCodes');
+  expect(result.summary.pCodes).toStrictEqual([
+    'P210',
+    'P233',
+    'P240',
+    'P241',
+    'P242',
+    'P243',
+    'P261',
+    'P264',
+    'P271',
+    'P273',
+    'P280',
+    'P302+P352',
+    'P303+P361+P353',
+    'P304+P340',
+    'P305+P351+P338',
+    'P312',
+    'P321',
+    'P332+P313',
+    'P337+P313',
+    'P362',
+    'P370+P378',
+    'P391',
+    'P403+P233',
+    'P403+P235',
+    'P405',
+    'P501',
+  ]);
+  expect(result).toHaveProperty('summary.pictograms');
+  expect(result.summary.pictograms).toStrictEqual(['GHS02', 'GHS07', 'GHS09']);
+  expect(result).toHaveProperty('summary.hStatements');
+  expect(result).toHaveProperty('summary.pStatements');
+});
 
-        value: 'GHS02',
-      },
-      {
-        label: 'Irritant',
-        url: 'https://pubchem.ncbi.nlm.nih.gov/images/ghs/GHS07.svg',
-
-        value: 'GHS07',
-      },
-      {
-        label: 'Environmental Hazard',
-        url: 'https://pubchem.ncbi.nlm.nih.gov/images/ghs/GHS09.svg',
-
-        value: 'GHS09',
-      },
-    ],
+test('getGHS detail request', () => {
+  let result = getGHS(data, { returnDetails: true, returnReferences: true });
+  expect(result).toHaveProperty('summary.hCodes');
+  expect(result.summary.hCodes).toStrictEqual([
+    'H226',
+    'H315',
+    'H319',
+    'H335',
+    'H411',
+  ]);
+  expect(result).toHaveProperty('summary.pCodes');
+  expect(result.summary.pCodes).toStrictEqual([
+    'P210',
+    'P233',
+    'P240',
+    'P241',
+    'P242',
+    'P243',
+    'P261',
+    'P264',
+    'P271',
+    'P273',
+    'P280',
+    'P302+P352',
+    'P303+P361+P353',
+    'P304+P340',
+    'P305+P351+P338',
+    'P312',
+    'P321',
+    'P332+P313',
+    'P337+P313',
+    'P362',
+    'P370+P378',
+    'P391',
+    'P403+P233',
+    'P403+P235',
+    'P405',
+    'P501',
+  ]);
+  expect(result).toHaveProperty('summary.pictograms');
+  expect(result.summary.pictograms).toStrictEqual(['GHS02', 'GHS07', 'GHS09']);
+  expect(result).toHaveProperty('summary.hStatements');
+  expect(result).toHaveProperty('summary.pStatements');
+  expect(result).toHaveProperty('details');
+  expect(result).toHaveProperty('references');
+  expect(result.details).toHaveProperty('hCodes');
+  expect(result.details).toHaveProperty('pCodes');
+  expect(result.details.pictograms).toStrictEqual({
+    6: ['GHS02', 'GHS07', 'GHS09'],
   });
+  expect(Object.keys(result.references)).toStrictEqual(['6']);
 });
