8c63511e7417eeb13f57ce59ad6911f86aa5fe6b
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-01-19 14:29:53 +0100

    docs: updated readme with link to api docs

--- src/compoundData/__tests__/getExperimentalData.test.js
+++ src/compoundData/__tests__/getExperimentalData.test.js
@@ -1,8 +1,34 @@
-import { getBoilingPoint } from '../getExperimentalData.js';
+import {
+  getBoilingPoint,
+  getMeltingPoint,
+  getSolubility,
+} from '../getExperimentalData.js';
 
 import data from './test.json';
 
 test('parsing experimental boiling point', () => {
   let result = getBoilingPoint(data);
-  console.log(result);
+  expect(result.summary).toStrictEqual({
+    mean: 129.8,
+    median: 129.8,
+    standardDeviation: NaN,
+  });
+});
+
+test('parsing experimental melting point', () => {
+  let result = getMeltingPoint(data);
+  expect(result.summary).toStrictEqual({
+    mean: -95,
+    median: -95,
+    standardDeviation: NaN,
+  });
+});
+
+test('parsing experimental solubility', () => {
+  let result = getSolubility(data);
+  expect(result.summary).toStrictEqual({
+    mean: 0.000841,
+    median: 0.000841,
+    standardDeviation: NaN,
+  });
 });
