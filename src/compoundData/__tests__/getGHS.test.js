103ecbced961f60b1cae2cac6a530078a6f1edb3
Luc Patiny <luc@patiny.com>
2021-01-29 17:48:21 +0100

    feat: refactor getGHS and add getGHSSummary

--- src/compoundData/__tests__/getGHS.test.js
+++ src/compoundData/__tests__/getGHS.test.js
@@ -1,101 +1,82 @@
-import { getGHS } from '../getGHS.js';
+import { getGHS, getGHSSummary } from '../getGHS.js';
 
-import data from './test.json';
+import bromopentane from './bromopentane.json';
+import triethylamine from './triethylamine.json';
 
-test('getGHS default request', () => {
-  let result = getGHS(data);
-  expect(result).toHaveProperty('summary.h');
-  let hCodes = result.summary.h.reduce((hCodes, entry) => {
-    hCodes.push(entry.code);
-    return hCodes;
-  }, []);
-  expect(hCodes).toStrictEqual(['H226', 'H315', 'H319', 'H335', 'H411']);
-  expect(result).toHaveProperty('summary.p');
-  let pCodes = result.summary.p.reduce((pCodes, entry) => {
-    pCodes.push(entry.code);
-    return pCodes;
-  }, []);
-  expect(pCodes).toStrictEqual([
-    'P210',
-    'P233',
-    'P240',
-    'P241',
-    'P242',
-    'P243',
-    'P261',
-    'P264',
-    'P271',
-    'P273',
-    'P280',
-    'P302+P352',
-    'P303+P361+P353',
-    'P304+P340',
-    'P305+P351+P338',
-    'P312',
-    'P321',
-    'P332+P313',
-    'P337+P313',
-    'P362',
-    'P370+P378',
-    'P391',
-    'P403+P233',
-    'P403+P235',
-    'P405',
-    'P501',
-  ]);
-  expect(result).toHaveProperty('summary.pictograms');
-  expect(result.summary.pictograms).toStrictEqual(['GHS02', 'GHS07', 'GHS09']);
+describe('getGHS', () => {
+  it('triethylamine', () => {
+    let result = getGHS(triethylamine);
+
+    expect(result.pictograms).toHaveLength(6);
+    expect(result.hStatements).toHaveLength(6);
+    expect(result.pStatements).toHaveLength(6);
+
+    expect(result.pictograms[0].data).toStrictEqual([
+      { code: 'GHS02', description: 'Flammable' },
+      { code: 'GHS05', description: 'Corrosive' },
+      { code: 'GHS07', description: 'Harmful Irritant' },
+    ]);
+
+    expect(result).toMatchSnapshot();
+  });
 });
 
-test('getGHS detail request', () => {
-  let result = getGHS(data, { returnDetails: true, returnReferences: true });
-  expect(result).toHaveProperty('summary.h');
-  let hcodes = result.summary.h.reduce((hCodes, entry) => {
-    hCodes.push(entry.code);
-    return hCodes;
-  }, []);
-  expect(hcodes).toStrictEqual(['H226', 'H315', 'H319', 'H335', 'H411']);
-  expect(result).toHaveProperty('summary.p');
-  let pCodes = result.summary.p.reduce((pCodes, entry) => {
-    pCodes.push(entry.code);
-    return pCodes;
-  }, []);
-  expect(pCodes).toStrictEqual([
-    'P210',
-    'P233',
-    'P240',
-    'P241',
-    'P242',
-    'P243',
-    'P261',
-    'P264',
-    'P271',
-    'P273',
-    'P280',
-    'P302+P352',
-    'P303+P361+P353',
-    'P304+P340',
-    'P305+P351+P338',
-    'P312',
-    'P321',
-    'P332+P313',
-    'P337+P313',
-    'P362',
-    'P370+P378',
-    'P391',
-    'P403+P233',
-    'P403+P235',
-    'P405',
-    'P501',
-  ]);
-  expect(result).toHaveProperty('summary.pictograms');
-  expect(result.summary.pictograms).toStrictEqual(['GHS02', 'GHS07', 'GHS09']);
-  expect(result).toHaveProperty('details');
-  expect(result).toHaveProperty('references');
-  expect(result.details).toHaveProperty('hCodes');
-  expect(result.details).toHaveProperty('pCodes');
-  expect(result.details.pictograms).toStrictEqual({
-    6: ['GHS02', 'GHS07', 'GHS09'],
+describe('getGHSSummry', () => {
+  it('bromopentane', () => {
+    let result = getGHSSummary(bromopentane);
+
+    expect(result.pictograms.map((entry) => entry.code)).toStrictEqual([
+      'GHS02',
+      'GHS07',
+      'GHS09',
+    ]);
+
+    expect(result.hStatements.map((entry) => entry.code)).toStrictEqual([
+      'H226',
+      'H315',
+      'H319',
+      'H335',
+      'H411',
+    ]);
+
+    expect(result.pStatements.map((entry) => entry.code)).toStrictEqual([
+      'P210',
+      'P233',
+      'P240',
+      'P241',
+      'P242',
+      'P243',
+      'P261',
+      'P264',
+      'P271',
+      'P273',
+      'P280',
+      'P302+P352',
+      'P303+P361+P353',
+      'P304+P340',
+      'P305+P351+P338',
+      'P312',
+      'P321',
+      'P332+P313',
+      'P337+P313',
+      'P362',
+      'P370+P378',
+      'P391',
+      'P403+P233',
+      'P403+P235',
+      'P405',
+      'P501',
+    ]);
   });
-  expect(Object.keys(result.references)).toStrictEqual(['6']);
+
+  it('triethylamine', () => {
+    let result = getGHSSummary(triethylamine);
+    expect(result.pictograms.map((entry) => entry.code)).toStrictEqual([
+      'GHS02',
+      'GHS05',
+      'GHS06',
+      'GHS07',
+      'GHS08',
+    ]);
+  });
 });
--- src/compoundData/getGHS.js
+++ src/compoundData/getGHS.js
@@ -1,9 +1,9 @@
 import jp from 'jsonpath';
 
 import { getReferences } from './getReferences.js';
+import { ghsPictogramText } from './ghsPictograms';
 import { hazardStatements } from './hazardStatements.js';
 import { precautionaryStatements } from './precautionaryStatements.js';
-
 /**
  *Remove duplicates objects of arrays via set
  *
@@ -22,15 +22,15 @@ function removeDuplicates(object) {
 /**
  * @typedef {Object} GHSData
  * @property {Object} summary
- * @property {Array.<String>} summary.pictograms - Array of unique GHS pictogram names, e.g., ["GHS01", "GHS02"]
- * @property {Array.<String>} summary.hCodes - Array of unique hCodes, e.g., ["H226", "H315"]
- * @property {Array.<String>} summary.hStatements - Array of unique hStatements, e.g., ["Flammable liquid and vapour", "Causes skin irritation"]
- * @property {Array.<String>} summary.pCodes - Array of unique pCodes, e.g., ["P210", "P233"]
- * @property {Array.<String>} summary.pStatements - Array of unique pStatements, e.g., ["Keep away from heat, hot surfaces, sparks, open flames and other ignition sources. No smoking. [As modified by IV ATP]"]
+ * @property {Array<String>} summary.pictograms - Array of unique GHS pictogram names, e.g., ["GHS01", "GHS02"]
+ * @property {Array<String>} summary.hStatements - Array of unique hStatements, e.g., ["H226", "H315"]
+ * @property {Array<String>} summary.hStatements - Array of unique hStatements, e.g., ["Flammable liquid and vapour", "Causes skin irritation"]
+ * @property {Array<String>} summary.pStatements - Array of unique pStatements, e.g., ["P210", "P233"]
+ * @property {Array<String>} summary.pStatements - Array of unique pStatements, e.g., ["Keep away from heat, hot surfaces, sparks, open flames and other ignition sources. No smoking. [As modified by IV ATP]"]
  * @property {Object} details
- * @property {Object.<string, Array.<String>} details.pictograms - Keys are the reference numbers, values arrays of GHS pictograms reported by reference
- * @property {Object.<string, Array.<String>} details.hCodes - Keys are the reference numbers, values arrays of H codes reported by reference
- * @property {Object.<string, Array.<String>} details.pCodes -Keys are the reference numbers, values arrays of P codes reported by reference
+ * @property {Object.<string, Array<String>} details.pictograms - Keys are the reference numbers, values arrays of GHS pictograms reported by reference
+ * @property {Object.<string, Array<String>} details.hStatements - Keys are the reference numbers, values arrays of H codes reported by reference
+ * @property {Object.<string, Array<String>} details.pStatements -Keys are the reference numbers, values arrays of P codes reported by reference
  * @property {Object} references
  * @property {String} references.url -
  * @property {String} references.sourceName -
@@ -43,15 +43,12 @@ function removeDuplicates(object) {
  *
  * @export
  * @param {Object} data response of a compound data request to the PubChem API
- * @param {Object} options
- * @param {Boolean} options.returnDetails if true also return a detailed results (H/P statements and pictograms for every reference). Defaults to false.
- * @param {Boolean} options.returnReferences if true also return the bioliographic details of the relevant references. Defaults to false.
  * @returns {GHSData}
  */
-export function getGHS(data, options = {}) {
-  const { returnDetails = false, returnReferences = false } = options;
+export function getGHS(data) {
+  const references = getReferences(data);
 
-  let allPictograms = jp
+  let pictograms = jp
     .query(
       data,
       '$.Section[?(@.TOCHeading==="Safety and Hazards")]' +
@@ -59,14 +56,20 @@ export function getGHS(data, options = {}) {
       '.Section[?(@.TOCHeading==="GHS Classification")]' +
       '.Information[?(@.Name==="Pictogram(s)")]',
     )
-    .reduce((pictogramDict, entry) => {
-      pictogramDict[entry.ReferenceNumber] = jp
+    .map((entry) => ({
+      reference: references[entry.ReferenceNumber],
+      data: jp
         .query(entry, '$.Value.StringWithMarkup[*].Markup[*]')
-        .map((entry) => entry.URL.match(/GHS\d+/)[0]);
-      return pictogramDict;
-    }, {});
+        .map((entry) => {
+          let code = entry.URL.match(/GHS\d+/)[0];
+          return {
+            code,
+            description: ghsPictogramText[code],
+          };
+        }),
+    }));
 
-  let allHCodes = jp
+  let hStatements = jp
     .query(
       data,
       '$.Section[?(@.TOCHeading==="Safety and Hazards")]' +
@@ -74,16 +77,20 @@ export function getGHS(data, options = {}) {
       '.Section[?(@.TOCHeading==="GHS Classification")]' +
       '.Information[?(@.Name==="GHS Hazard Statements")]',
     )
-    .reduce((hCodeDict, entry) => {
-      hCodeDict[entry.ReferenceNumber] = jp
+    .map((entry) => ({
+      reference: references[entry.ReferenceNumber],
+      data: jp
         .query(entry, '$.Value.StringWithMarkup[*]')
-        .map((entry) => entry.String.match(/H\d+/)[0]);
-      return hCodeDict;
-    }, {});
+        .map((entry) => entry.String.match(/H\d+/)[0])
+        .map((hCode) => ({
+          code: hCode,
+          description: hazardStatements[hCode],
+        })),
+    }));
 
   //ToDo(kjappelbaum): investigate in more detail why they do not have the full P statements
   //For P statements the full sentence (with conditions) is more important than just the number
-  let allPCodes = jp
+  let pStatements = jp
     .query(
       data,
       '$.Section[?(@.TOCHeading==="Safety and Hazards")]' +
@@ -91,68 +98,48 @@ export function getGHS(data, options = {}) {
       '.Section[?(@.TOCHeading==="GHS Classification")]' +
       '.Information[?(@.Name==="Precautionary Statement Codes")]',
     )
-    .reduce((pCodeDict, entry) => {
-      pCodeDict[entry.ReferenceNumber] = jp
+    .map((entry) => ({
+      reference: references[entry.ReferenceNumber],
+      data: jp
         .query(entry, '$.Value.StringWithMarkup[*]')
-        .reduce((filtered, entry) => {
-          let res = entry.String.match(
+        .map((entry) =>
+          entry.String.match(
             /(?<oneP>(?<!\+)P\d\d\d(?!\+))|(?<twoP>P\d\d\d\+P\d\d\d(?!\+))|(?<threeP>P\d\d\d\+P\d\d\d\+P\d\d\d(?!\+))/gm,
-          );
-          if (res) filtered.push(...res);
-          return filtered;
-        }, []);
-      return pCodeDict;
-    }, {});
-  const uniquePictograms = removeDuplicates(allPictograms);
+          ),
+        )[0]
+        .map((pCode) => ({
+          code: pCode,
+          description: precautionaryStatements[pCode],
+        })),
+    }));
 
-  let relevantReferences = new Set();
-
-  [allPictograms, allHCodes, allPCodes].forEach((list) =>
-    Object.keys(list).forEach(relevantReferences.add, relevantReferences),
-  );
-
-  const uniqueHCodes = removeDuplicates(allHCodes);
-
-  const uniquePCodes = removeDuplicates(allPCodes);
-
-  const references = getReferences(data);
-
-  let h = uniqueHCodes.reduce((hStatementList, entry) => {
-    hStatementList.push({ code: entry, statement: hazardStatements[entry] });
-    return hStatementList;
-  }, []);
-
-  let p = uniquePCodes.reduce((pStatementList, entry) => {
-    pStatementList.push({
-      code: entry,
-      statement: precautionaryStatements[entry],
-    });
-    return pStatementList;
-  }, []);
-
-  let output = {
-    summary: {
-      pictograms: uniquePictograms,
-      h: h,
-      p: p,
-    },
+  return {
+    pictograms,
+    hStatements,
+    pStatements,
   };
+}
 
-  if (returnDetails) {
-    output.details = {
-      pictograms: allPictograms,
-      hCodes: allHCodes,
-      pCodes: allPCodes,
-    };
-  }
+/**
+ * We combine all the results and keep
+ */
+export function getGHSSummary(data) {
+  let result = getGHS(data);
+  return {
+    pictograms: getUnique(result.pictograms),
+    hStatements: getUnique(result.hStatements),
+    pStatements: getUnique(result.pStatements),
+  };
+}
 
-  if (returnReferences) {
-    output.references = Object.keys(references)
-      .filter((key) => relevantReferences.has(key))
-      .reduce((obj, key) => {
-        obj[key] = references[key];
-        return obj;
-      }, {});
-  }
-  return output;
+function getUnique(entries) {
+  let uniqueMap = {};
+  entries.map((entry) =>
+    entry.data.forEach((line) => {
+      uniqueMap[line.code] = line;
+    }),
+  );
+  return Object.keys(uniqueMap)
+    .map((code) => uniqueMap[code])
+    .sort((a, b) => (a.code < b.code ? -1 : 1));
 }
