26cdeba8ffb9db98deabf6c07db312df5e00bfea
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-03-11 17:15:05 +0100

    return null result in case there is an issue with computed data

--- src/compoundData/computed/getNumberProperties.js
+++ src/compoundData/computed/getNumberProperties.js
@@ -3,22 +3,29 @@ import { getComputedPropertySection } from './getComputedPropertySection.js';
 
 export function getNumberProperties(data, sectionName) {
   const computationalData = getComputedDataSection(data);
-  let section = getComputedPropertySection(
-    computationalData[0],
-    sectionName,
-  )[0];
 
   let output = {
-    value: section.Value.Number[0],
-    label: sectionName,
+    value: null,
+    label: null,
     reference: {
-      description: section.Reference[0],
+      description: null,
     },
   };
 
-  if (section.Value.Unit) {
-    output.units = section.Value.Unit;
-  }
+  try {
+    let section = getComputedPropertySection(
+      computationalData[0],
+      sectionName,
+    )[0];
+
+    output.value = section.Value.Number[0];
+    output.label = sectionName;
+    output.reference.description = section.Reference[0];
+    if (section.Value.Unit) {
+      output.units = section.Value.Unit;
+    }
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
 
   return output;
 }
