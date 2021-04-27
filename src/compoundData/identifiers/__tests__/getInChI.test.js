c4145ba0224b4b9a425620c410385d21b7ee2843
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-04-27 16:29:40 +0200

    feat: add InChI

---
+++ src/compoundData/identifiers/__tests__/getInChI.test.js
@@ -0,0 +1,23 @@
+import bromopentane from '../../../../data/bromopentane.json';
+import ethyleneglycol from '../../../../data/ethyleneglycol.json';
+import triethylamine from '../../../../data/triethylamine.json';
+import { getInChI } from '../getInChI.js';
+
+describe('get InChI', () => {
+  it('bromopentane', () => {
+    expect(getInChI(bromopentane)).toStrictEqual(
+      'InChI=1S/C5H11Br/c1-2-3-4-5-6/h2-5H2,1H3',
+    );
+  });
+  it('ethyleneglycol', () => {
+    expect(getInChI(ethyleneglycol)).toStrictEqual(
+      'InChI=1S/C2H6O2/c3-1-2-4/h3-4H,1-2H2',
+    );
+  });
+
+  it('triethylamine', () => {
+    expect(getInChI(triethylamine)).toStrictEqual(
+      'InChI=1S/C6H15N/c1-4-7(5-2)6-3/h4-6H2,1-3H3',
+    );
+  });
+});
