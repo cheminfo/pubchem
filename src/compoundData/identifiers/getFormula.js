98c64db65f881d5cb82096a0aedbcb8307dd2247
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-04-29 12:15:57 +0200

    feat: reformat identifier result

--- src/compoundData/identifiers/getFormula.js
+++ src/compoundData/identifiers/getFormula.js
@@ -1,10 +1,18 @@
 import jp from 'jsonpath';
 
 export function getFormula(data) {
-  return jp.query(
-    data,
-    '$.Section[?(@.TOCHeading==="Names and Identifiers")]' +
-      '.Section[?(@.TOCHeading==="Molecular Formula")]' +
-      '.Information[0]',
-  )[0].Value.StringWithMarkup[0].String;
+  return {
+    value: jp.query(
+      data,
+      '$.Section[?(@.TOCHeading==="Names and Identifiers")]' +
+        '.Section[?(@.TOCHeading==="Molecular Formula")]' +
+        '.Information[0]',
+    )[0].Value.StringWithMarkup[0].String,
+    label: 'Molecular Formula',
+    description: jp.query(
+      data,
+      '$.Section[?(@.TOCHeading==="Names and Identifiers")]' +
+        '.Section[?(@.TOCHeading==="Molecular Formula")]',
+    )[0].Description,
+  };
 }
